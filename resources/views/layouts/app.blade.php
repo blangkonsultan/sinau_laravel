9d53b40331b59fc694a30c353f5fe36b1053c1e3
blangkonsultan <bagaspbsc@gmail.com>
2022-08-29 14:24:52 +0700

    apdet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,13 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.2/dist/flowbite.min.css" />
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script src="https://cdn.tailwindcss.com"></script>
         <script src="https://unpkg.com/flowbite@1.5.2/dist/datepicker.js"></script>
+        <script src="https://unpkg.com/flowbite@1.5.2/dist/flowbite.js"></script>
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
